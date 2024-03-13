b08d95a10c15102d165423654b97df79b9034ea7
hushuai.p <hushuai001@163.com>
2024-03-14 02:14:48 +0800

    dev(hus):fix comment by paparazz and load model only once

--- CogVLM/basic_demo/cli_demo_sqa.py
+++ CogVLM/basic_demo/cli_demo_sqa.py
@@ -8,79 +8,78 @@ from sat.model import AutoModel
 import argparse
 
 from utils.utils import chat, llama2_tokenizer, llama2_text_processor_inference, get_image_processor
+from utils.models import CogAgentModel, CogVLMModel
+class cogvlm_model():
 
-def CogVLM_inference(img_path,question):
+    def __init__(self):
+        local_model_path = os.environ['COGVLM_MODEL_PATH']
+        self.parse_args = argparse.Namespace(
+            max_length=2048,
+            top_p=0.4,
+            topk=1,
+            temperature=0.8,
+            chinese=True,
+            version='chat_old',
+            quant=None,
+            from_pretrained='cogagent-vqa',
+            local_tokenizer='lmsys/vicuna-7b-v1.5',
+            fp16=False,
+            bf16=True,
+            stream_chat=True)
+        
+        model, model_args = AutoModel.from_pretrained(
+            'cogagent-vqa',
+            args=argparse.Namespace(
+            deepspeed=None,
+            local_rank=0,
+            world_size=1,
+            model_parralel_size=1,
+            mode='inference',
+            skip_init=True,
+            rank=0,
+            use_gpu_initialization=True,
+            device='cuda',
+            **vars(self.parse_args)
+            ),
+            home_path = local_model_path,)
+        self.model = model.eval()
+        language_processor_version = model_args.text_processor_version if 'text_processor_version' in model_args else self.parse_args.version
+        tokenizer = llama2_tokenizer("lmsys/vicuna-7b-v1.5", signal_type=language_processor_version)
+        self.image_processor = get_image_processor(model_args.eva_args["image_size"][0])
+        self.cross_image_processor = get_image_processor(model_args.cross_image_pix) if "cross_image_pix" in model_args else None
+        self.model.add_mixin('auto-regressive', CachedAutoregressiveMixin())
+        self.text_processor_infer = llama2_text_processor_inference(tokenizer, 2048, self.model.image_length)
+        
 
-    local_model_path = os.environ['COGVLM_MODEL_PATH']
-    
-    parse_args = argparse.Namespace(
-        max_length=2048,
-        top_p=0.4,
-        topk=1,
-        temperature=0.8,
-        chinese=True,
-        version='chat_old',
-        quant=None,
-        from_pretrained='cogagent-vqa',
-        local_tokenizer='lmsys/vicuna-7b-v1.5',
-        fp16=False,
-        bf16=True,
-        stream_chat=True)
-    
-    model, model_args = AutoModel.from_pretrained(
-        'cogagent-vqa',
-        args=argparse.Namespace(
-        deepspeed=None,
-        local_rank=0,
-        world_size=1,
-        model_parralel_size=1,
-        mode='inference',
-        skip_init=True,
-        rank=0,
-        use_gpu_initialization=True,
-        device='cuda',
-        **vars(parse_args)
-        ),
-        home_path = local_model_path,)
-    model = model.eval()
 
-    language_processor_version = 'chat_old'
-    tokenizer = llama2_tokenizer("lmsys/vicuna-7b-v1.5", signal_type=language_processor_version)
-    image_processor = get_image_processor(model_args.eva_args["image_size"][0])
-    cross_image_processor = None
+    def CogVLM_inference(self,img_path,question):
+        with torch.no_grad():
+                history = None
+                cache_image = None
+                assert img_path is not None
+                query = question
+                assert query is not None 
+                
+                try:
+                    response, history, cache_image = chat(
+                        img_path,
+                        self.model,
+                        self.text_processor_infer,
+                        self.image_processor,
+                        query,
+                        history=history,
+                        cross_img_processor=self.cross_image_processor,
+                        image=cache_image,
+                        max_length=2048,
+                        top_p=0.4,
+                        temperature=.8,
+                        top_k=1,
+                        invalid_slices=self.text_processor_infer.invalid_slices,
+                        args=self.parse_args
+                        )
+                except Exception as e:
+                    print(e)
 
-    model.add_mixin('auto-regressive', CachedAutoregressiveMixin())
-
-    text_processor_infer = llama2_text_processor_inference(tokenizer, 2048, model.image_length)
-
-    with torch.no_grad():
-            history = None
-            cache_image = None
-            assert img_path is not None
-
-            query = question
-            assert query is not None 
-            
-            try:
-                response, history, cache_image = chat(
-                    img_path,
-                    model,
-                    text_processor_infer,
-                    image_processor,
-                    query,
-                    history=history,
-                    cross_img_processor=cross_image_processor,
-                    image=cache_image,
-                    max_length=2048,
-                    top_p=0.4,
-                    temperature=.8,
-                    top_k=1,
-                    invalid_slices=text_processor_infer.invalid_slices,
-                    args=parse_args
-                    )
-            except Exception as e:
-                print(e)
-
-            return response
+                return response
 
 
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 import uuid
 import oss2
 from oss2.credentials import EnvironmentVariableCredentialsProvider
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from CogVLM.basic_demo.cli_demo_sqa import CogVLM_inference
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from CogVLM.basic_demo.cli_demo_sqa import cogvlm_model
+
+
 app = FastAPI()
 
 upload_dir = './upload/'
@@ -14,6 +16,21 @@ legal_extensions = ['png', 'jpg', 'jpeg']
 endpoint = os.environ["OSS_ENDPOINT"]
 auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
 bucket = oss2.Bucket(auth, endpoint, f'{os.environ["BUCKET_NAME"]}')
+
+_model_instance: cogvlm_model = None
+
+async def get_cogvlm_model() -> cogvlm_model:
+    global _model_instance
+    if _model_instance is None:
+        _model_instance = cogvlm_model()
+    return _model_instance
+
+@app.on_event("startup")
+async def startup_event():
+    global _model_instance
+    if _model_instance is None:
+        _model_instance = await get_cogvlm_model()
+
 
 @app.get('/')
 async def hello_world():
@@ -49,10 +66,9 @@ async def upload_image(file:UploadFile = File(...)):
             )
         
 @app.get('/resnet')
-async def resnet_inference(task_id: str, topk_result: int):
+async def resnet_inference(task_id: str, topk_result: int,):
     for file_extension in legal_extensions:
-        file_name = f'{task_id}.{file_extension}'
-        local_file_path = f'./upload/{file_name}'
+        local_file_path = f'./upload/{task_id}.{file_extension}'
         bucket.get_object_to_file(f'{os.environ["BUCKET_UPLOAD_PATH"]}', local_file_path)
         if os.path.exists(local_file_path):
             inference_result = predict_class(local_file_path,topk_result)
@@ -64,18 +80,15 @@ async def resnet_inference(task_id: str, topk_result: int):
             raise HTTPException(
                     status_code = 404,
                     detail = f'{task_id} task not found'
-                )
+               )
 
 @app.post('/cogvlm')
-async def resnet_inference(task_id: str, question: str):
+async def cogvlm_inference(task_id: str, question: str, cogvlm_model: cogvlm_model = Depends(get_cogvlm_model)):
     for file_extension in legal_extensions:
-        file_name = f'{task_id}.{file_extension}'
-        local_file_path = f'./upload/{file_name}'
+        local_file_path = f'./upload/{task_id}.{file_extension}'
         bucket.get_object_to_file(f'{os.environ["BUCKET_UPLOAD_PATH"]}', local_file_path)
         if os.path.exists(local_file_path):
-            img_path = []
-            img_path.append(local_file_path)
-            inference_result = CogVLM_inference(img_path,question) 
+            inference_result = cogvlm_model.CogVLM_inference(local_file_path,question) 
             os.remove(local_file_path)
             return{
                     'result' : inference_result
@@ -85,3 +98,7 @@ async def resnet_inference(task_id: str, question: str):
                     status_code = 404,
                     detail = f'{task_id} task not found'
                 )
+
+if __name__ =='__main__':
+    import uvicorn 
+    uvicorn.run('main:app', host="0.0.0.0", port=8000,reload=True)
